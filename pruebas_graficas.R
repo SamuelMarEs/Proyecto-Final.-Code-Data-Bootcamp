fc72dee03c6d793bca7a8bf6b05d0695169cb910
GeroChavez <gerochavezram@gmail.com>
2026-06-27 18:46:06 -0600

    Actualicé el rscript donde están las gráficas hechas hasta el momento, hice una matriz de correlación de distribución de datos entre el año y el numero de goles que se hizo en el mundial de local y de visitante

--- pruebas_graficas.R
+++ pruebas_graficas.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(ggplot2)
+library(GGally)
 
 # Carga de datos
 Partidos <- read.csv("wcmatches.csv")
@@ -13,11 +14,11 @@ dias_ordenados <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sat
 Partidos$dayofweek <- factor(Partidos$dayofweek, levels = dias_ordenados)
 
 # Una variable d los goles totales por partido
-Partidos$total_goals <- Partidos$home_score + Partidos$away_score
+Partidos$goles_totales <- Partidos$home_score + Partidos$away_score
 
 # ALGUNAS GRÁFICAS GENERALES
 # Gráfica A: Histograma de goles totales
-grafica_goles <- ggplot(Partidos, aes(x = total_goals)) +
+grafica_goles <- ggplot(Partidos, aes(x = goles_totales)) +
   geom_histogram(binwidth = 1, fill = "darkgreen", color = "white", alpha = 0.9) +
   labs(
     title = "Distribución de Goles Totales por Partido (1930 - 2018)",
@@ -49,3 +50,35 @@ grafica_partidos <- Partidos %>%
     panel.grid.minor = element_blank()
   )
 grafica_partidos
+
+# Distribución de datos, matriz de correlación
+
+# Filtracion de los datos y renombramiento pa q salgan en español al imprimir
+datos_numericos <- Partidos[, c("year", "home_score", "away_score")]
+colnames(datos_numericos) <- c("Año del Mundial", "Goles Local", "Goles Visitante")
+
+# impresión de la matriz de correlación
+matriz_correlacion <- ggpairs(
+  datos_numericos,
+  # Escribirmos lo mismo arriba y abajo de la matriz para ver el comportamiento en el cruce de variables
+  upper = list(continuous = wrap("smooth", alpha = 0.3, color = "#2c3e50")), 
+  lower = list(continuous = wrap("smooth", alpha = 0.3, color = "#2c3e50")), 
+  # curvas de densidad en la diagonal
+  diag = list(continuous = wrap("densityDiag", fill = "#16a085", alpha = 0.6)),
+  
+  title = "Distribución de datos y relación entre año y goles en las ediciones de los mundiales (1930-2018)"
+) +
+  theme_minimal(base_size = 12) +
+  theme(
+    plot.title = element_text(face = "bold", size = 15, hjust = 0.5),
+    strip.background = element_rect(fill = "#ecf0f1", color = "white"),
+    strip.text = element_text(face = "bold", size = 10)
+  )
+matriz_correlacion
+
+# TABLA DE CORRELACIÓN DE PEARSON
+# usamos la funcion y calculamos la matriz matemática y redondeamos a 3 decimales
+matriz_pearson <- round(cor(datos_numericos), 3)
+# Imprimimos la tabla en la consola
+print("Coeficientes de Correlación de Pearson:")
+matriz_pearson
